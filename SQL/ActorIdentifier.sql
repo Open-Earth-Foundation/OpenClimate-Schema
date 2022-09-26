79b9e40bde7c943e2f2b054e741fcb0ff67dd5b8
Evan Prodromou <evan@openearth.org>
2022-09-25 22:44:55 -0400

    fix(ActorIdentifer): fkey to Actor; closes #28

--- SQL/ActorIdentifier.sql
+++ SQL/ActorIdentifier.sql
@@ -2,5 +2,8 @@ CREATE TABLE `ActorIdentifier` (
   `actor_id` varchar(255),
   `identifier` varchar(255),
   `namespace` varchar(255),
-  PRIMARY KEY (`identifier`, `namespace`)
+  PRIMARY KEY (`identifier`, `namespace`),
+  CONSTRAINT "FK_ActorIdentfier.actor_id"
+    FOREIGN KEY ("actor_id")
+      REFERENCES "Actor"("actor_id"),
 );
