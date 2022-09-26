4f800c4ddb386199d84cee51bc0b746b8a2c2cf3
Evan Prodromou <evan@openearth.org>
2022-09-25 22:42:56 -0400

    feat(ActorIdentifier): new pkey; closes #27

--- SQL/ActorIdentifier.sql
+++ SQL/ActorIdentifier.sql
@@ -2,5 +2,5 @@ CREATE TABLE `ActorIdentifier` (
   `actor_id` varchar(255),
   `identifier` varchar(255),
   `namespace` varchar(255),
-  PRIMARY KEY (`actor_id`, `identifier`, `namespace`)
+  PRIMARY KEY (`identifier`, `namespace`)
 );
