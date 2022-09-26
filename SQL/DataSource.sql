40df05c021e87a95106b8af248bcb067dff2b9fe
Evan Prodromou <evan@openearth.org>
2022-09-25 22:55:35 -0400

    fix(sql): duplicate last_updated in DataSource

--- SQL/DataSource.sql
+++ SQL/DataSource.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `DataSource` (
   `datasource_id` varchar(255),
   `name` varchar(255),
-  `last_updated` datetime,
   `publisher` varchar(255),
   `published` datetime,
   `URL` varchar(255),
