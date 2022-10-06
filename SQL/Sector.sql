0936c449778dc60a73591802a70a8c8ea3c79db1
Evan Prodromou <evan@openearth.org>
2022-10-06 12:36:35 -0400

    fix(sql): SQL syntax error in Sector.sql
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- SQL/Sector.sql
+++ SQL/Sector.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Sector" (
-  "sector_id" varchar(255), /* Unique ID; 
-  "name" varchar(255),
-  "namespace" varchar(255),
+  "sector_id" varchar(255), /* Unique ID */
+  "name" varchar(255), /* human-readable name; TODO: i18n */
+  "namespace" varchar(255), /* Namespace or vocabulary for the sector */
   "datasource_id" varchar(255), /* Where the record came from */
   "created" timestamp,
   "last_updated" timestamp,
