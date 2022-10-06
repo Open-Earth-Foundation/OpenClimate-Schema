898840ab44db1b287018ef21bf6fbbcd909c6c2e
Evan Prodromou <evan@openearth.org>
2022-10-06 12:19:14 -0400

    feat(emissions): move out scope, bigint for total
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- SQL/EmissionsAgg.sql
+++ SQL/EmissionsAgg.sql
@@ -1,16 +1,13 @@
-CREATE TABLE `EmissionsAgg` (
-  `emissions_id` varchar(255),
-  `actor_id` varchar(255),
-  `year` int,
-  `scope1_agg` int,
-  `scope2_agg` int,
-  `scope3_agg` int,
-  `total_emissions` int,
-  `methodology_id` varchar(255),
-  `datasource_id` varchar(255),
-  `created` timestamp,
-  `last_updated` timestamp,
-  PRIMARY KEY (`emissions_id`),
+CREATE TABLE "EmissionsAgg" (
+  "emissions_id" varchar(255), /* Unique identifier for this record */
+  "actor_id" varchar(255), /* Responsible party for the emissions */
+  "year" int, /* Year of emissions, YYYY */
+  "total_emissions" bigint, /* Integer value of tonnes of CO2 equivalent */
+  "methodology_id" varchar(255), /* Methodology used */
+  "datasource_id" varchar(255), /* Source for the data */
+  "created" timestamp,
+  "last_updated" timestamp,
+  PRIMARY KEY ("emissions_id"),
   CONSTRAINT "FK_EmissionsAgg.actor_id"
     FOREIGN KEY ("actor_id")
       REFERENCES "Actor"("actor_id"),
