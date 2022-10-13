f6b213cb1aa7ce58944a32b8f425015a50ccad65
yeozy95 <zhiyi.yeo@u.yale-nus.edu.sg>
2022-10-13 10:33:10 -0400

    fix(SQL): Replace backticks with double quotations

--- SQL/Target.sql
+++ SQL/Target.sql
@@ -1,16 +1,16 @@
-CREATE TABLE `Target` (
-  `target_id` varchar(255),
-  `actor_id` varchar(255),
-  `target_type` varchar(255),
-  `baseline_year` int,
-  `target_year` int,
-  `target_value` int,
-  `target_unit` varchar(255),
-  `bau_value` int,
-  `is_net_zero` boolean,
-  `percent_achieved` int,
-  `data_source` varchar(255),
-  `created` timestamp,
-  `last_updated` timestamp,
-  PRIMARY KEY (`target_id`)
+CREATE TABLE "Target" (
+  "target_id" varchar(255),
+  "actor_id" varchar(255),
+  "target_type" varchar(255),
+  "baseline_year" int,
+  "target_year" int,
+  "target_value" int,
+  "target_unit" varchar(255),
+  "bau_value" int,
+  "is_net_zero" boolean,
+  "percent_achieved" int,
+  "data_source" varchar(255),
+  "created" timestamp,
+  "last_updated" timestamp,
+  PRIMARY KEY ("target_id")
 );
