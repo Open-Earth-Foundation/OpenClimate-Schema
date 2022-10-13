cbf72534062509c669639095506b3ccc7a082ee9
yeozy95 <zhiyi.yeo@u.yale-nus.edu.sg>
2022-10-13 16:17:23 -0400

    feat(Target) Update based on learnings from CDP Cities harmonization

--- SQL/Target.sql
+++ SQL/Target.sql
@@ -9,8 +9,18 @@ CREATE TABLE "Target" (
   "bau_value" int,
   "is_net_zero" boolean,
   "percent_achieved" int,
+  "sector_list" varchar(255),
+  "baseline_emissions" varchar(255),
+  "target_year_emissions" varchar(255),
+  "target_boundary" varchar(255),
   "data_source" varchar(255),
   "created" timestamp,
   "last_updated" timestamp,
   PRIMARY KEY ("target_id")
+  CONSTRAINT "Target.baseline_emissions"
+    FOREIGN KEY ("baseline_emissions")
+      REFERENCES "EmissionsAgg"("emissions_id")
+  CONSTRAINT "Target.target_year_emissions"
+    FOREIGN KEY ("target_year_emissions")
+      REFERENCES "EmissionsAgg"("emissions_id")
 );
