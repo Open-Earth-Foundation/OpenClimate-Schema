5688f4ffe0cbe0abcfd412fe1507683e3082dee9
yeozy95 <zhiyi.yeo@u.yale-nus.edu.sg>
2022-10-13 10:45:13 -0400

    fix(EmissionsBreakdown): Remove unnecessary columns

--- SQL/EmissionsBreakdown.sql
+++ SQL/EmissionsBreakdown.sql
@@ -1,7 +1,5 @@
 CREATE TABLE "EmissionsBreakdown" (
   "emissions_id" varchar(255),
-  "actor_id" varchar(255),
-  "year" int,
   "emissions_scope" varchar(255),
   "emissions_source" varchar(255),
   "sector" varchar(255),
@@ -13,8 +11,6 @@ CREATE TABLE "EmissionsBreakdown" (
   "emissions_value" int,
   "reporting_boundary" varchar(255),
   "gwp_used" varchar(255),
-  "methodology_id" varchar(255),
-  "datasource_id" varchar(255),
   "created" timestamp,
   "last_updated" timestamp,
   PRIMARY KEY ("emissions_id")
