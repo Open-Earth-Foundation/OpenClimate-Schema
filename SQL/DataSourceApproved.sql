9f1148c1eb2dbf8ab18b2b8fddda935e55f905dc
Luke Gloege <lukegloege@gmail.com>
2023-03-03 15:20:30 -0500

    docs(ON-179): add explanation of ghg_target_percent_achieved field

--- SQL/DataSourceApproved.sql
+++ SQL/DataSourceApproved.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE "DataSourceApproved" (
   "datasource_id" varchar(255), /* Which data source */
-  "ghg_target_percent_achieved" REAL,
+  "ghg_target_percent_achieved" REAL, /* quality score between 0 - 1 */
   "notes" TEXT, /* why score chosen for each field */
   "created" timestamp,
   "last_updated" timestamp,
